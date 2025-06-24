aad4f5a7f8892408ff48eb62be015b5c6825d0bc
Cristi <cristibologa98@yahoo.com>
2025-06-24 15:14:40 +0300

    testimonials

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,34 @@ burgerButton.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   burgerMenu.classList.remove("is-active");
 });
+
+// cards
+const testimonialsCards = document.querySelector(".testimonials__cards");
+const testimonialCards = document.querySelectorAll(
+  ".testimonials__cards .card"
+);
+const leftBtn = document.querySelector(".button-left");
+const rightBtn = document.querySelector(".button-right");
+let testimonialIndex = 0;
+const cardWidth = 420; // card width + margin (adjust if needed)
+const totalTestimonials = testimonialCards.length;
+
+function updateTestimonialsSlide() {
+  testimonialsCards.style.transform = `translateX(-${
+    testimonialIndex * cardWidth
+  }px)`;
+}
+
+leftBtn.addEventListener("click", () => {
+  if (testimonialIndex < totalTestimonials - 1) {
+    testimonialIndex++;
+    updateTestimonialsSlide();
+  }
+});
+
+rightBtn.addEventListener("click", () => {
+  if (testimonialIndex > 0) {
+    testimonialIndex--;
+    updateTestimonialsSlide();
+  }
+});
